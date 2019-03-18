fd4ea8bc51eab205ec490a18d574e611ecd5201b
swapnil <oliqarrow123@gmail.com>
2019-03-18 18:52:46 +0530

    added check_db.py

---
+++ scripts/check_db.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+from configuartions import db_configs
+import pymysql as pml
+db_configs = {'user':'user','password':'password','host':'mysqlserver','port':3306}
+import time
+
+if __name__ == '__main__':
+
+    while True:
+        try:
+            conn = pml.connect()
+        except Exception as e:
+            print("connection exception: ")
+            print("cause:",str(e))
+            continue
+        try:
+            cursor = conn.cursor()
+            cursor.execute("SHOW DATABASES;")
+            print("DATABASES: ")
+            for row in cursor.fetchall():
+                print(row)
+        except Exception:
+            print("query execution exception: ")
+            print("cause:",str(e))
+        
+        finally:
+            conn.close()
+
+        time.sleep(30)
