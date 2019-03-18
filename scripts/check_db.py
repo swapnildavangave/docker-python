16beaeb092d3bdf5b2cf6721a19befe90550087f
swapnil <oliqarrow123@gmail.com>
2019-03-18 20:00:37 +0530

    added logging to check_db.py

--- scripts/check_db.py
+++ scripts/check_db.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 
 # from configuartions import db_configs
+import logging
 import pymysql as pml
 db_configs = {'user':'user','password':'password','host':'db','port':3306}
 import time
-
+logging.basicConfig(filename='/scripts/output.log',level=logging.DEBUG)
 if __name__ == '__main__':
 
     time.sleep(60)
@@ -12,10 +13,12 @@ if __name__ == '__main__':
         try:
             conn = pml.connect(host=db_configs['host'],user=db_configs['user']
             ,port=db_configs['port'],password=db_configs['password'])
+            logging.info("Connection made.")
         except Exception as e:
             print("connection exception: ")
             print("cause:",str(e))
-            time.sleep(30)        
+            time.sleep(30)
+            logging.warning(str(e))
             continue
         try:
             cursor = conn.cursor()
@@ -23,11 +26,14 @@ if __name__ == '__main__':
             print("DATABASES: ")
             for row in cursor.fetchall():
                 print(row)
-        except Exception:
+                logging.info('{}'.format(row))
+
+        except Exception as e:
+            logging.warning(str(e))
             print("query execution exception: ")
             print("cause:",str(e))
         
         finally:
             conn.close()
-
+            logging.info("Connection closed.")
         time.sleep(30)
