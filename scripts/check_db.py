ef2e267e354d44aae61100aaa4d81c0d8c815bec
swapnil <oliqarrow123@gmail.com>
2019-03-18 19:52:37 +0530

    made changes to check_db.py

--- scripts/check_db.py
+++ scripts/check_db.py
@@ -9,10 +9,12 @@ if __name__ == '__main__':
 
     while True:
         try:
-            conn = pml.connect()
+            conn = pml.connect(host=db_configs['host'],user=db_configs['user']
+            ,port=db_configs['port'],password=db_configs['password'])
         except Exception as e:
             print("connection exception: ")
             print("cause:",str(e))
+            time.sleep(30)        
             continue
         try:
             cursor = conn.cursor()
